6ebf77ff51a76b7775c30585014e80e739d31344
sifan <sdong@syr.edu>
2021-09-24 16:51:55 -0400

    1651

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const home = express.Router()
-home.get('/',(req,res)=>{
-    res.render('home')
+home.get('/',async(req,res)=>{
+    let user = res.locals.user
+    res.render('home',{
+        user:user
+    })
 })
 module.exports = home
